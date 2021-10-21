3a2732f0598c67add9f4ce89d360f7cb9ca7832e
Kali <michal.mendygrall@gmail.com>
2021-10-21 07:17:53 -0400

    sending and receiving packets

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+#https://scapy.readthedocs.io/en/latest/
 import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
+    #srp send_receive_packet returns answered and unanswered 
+    scapy.srp(arp_request_broadcast)
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary()) 
+
 
 scan("10.0.2.2/24")
